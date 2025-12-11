cf6edbe1d94aba9506faabe9e788d677a385c7bc
Tommy Yu <y@metatoaster.com>
2025-12-11 23:32:59 +1300

    Include the tests
    - These are again pulled directly from the `axum-login` project, but
      with the modification to also use the bearer token.
    - The intent is to show that these tests should continue to work when
      we enable the signed option.

---
+++ axum-login-bearer/tests/integration-test.rs
@@ -0,0 +1,146 @@
+use std::{
+    collections::HashMap,
+    process::{Child, Command},
+    sync::Arc,
+    time::{Duration, Instant},
+};
+
+use reqwest::{
+    cookie::{CookieStore, Jar},
+    header::{AUTHORIZATION, HeaderMap, HeaderValue},
+    Client, StatusCode, Url,
+};
+use serial_test::serial;
+
+const WEBSERVER_URL: &str = "http://localhost:3000";
+
+#[tokio::test]
+#[serial]
+async fn sqlite_bearer_example() {
+    let _child_guard = start_example_binary("example-sqlite-bearer").await;
+
+    let cookie_jar = Arc::new(Jar::default());
+    let client = Client::builder()
+        .cookie_provider(cookie_jar.clone())
+        .build()
+        .unwrap();
+
+    // A logged out user is redirected to the login URL with a next query
+    //string.
+    let res = client.get(url("/")).send().await.unwrap();
+    assert_eq!(*res.url(), url("/login?next=%2F"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    assert!(
+        cookie_jar.cookies(&url("/")).is_none(),
+        "Expected 'id' cookie to not be set after failed login"
+    );
+
+    // Log in with invalid credentials.
+    let res = login(&client, "ferris", "bogus").await;
+    assert_eq!(*res.url(), url("/login"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    let cookies = cookie_jar
+        .cookies(&url("/"))
+        .expect("A cookie should be set");
+    assert!(
+        cookies.to_str().unwrap_or("").contains("id="),
+        "Expected 'id' cookie to be set after login"
+    );
+
+    // Log in with valid credentials.
+    let res = login(&client, "ferris", "hunter42").await;
+    assert_eq!(*res.url(), url("/"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // extract the session id from the cookie set after the successful login
+    let cookies = cookie_jar
+        .cookies(&url("/"))
+        .expect("A cookie should be set");
+    let (_, session_id) = cookies.to_str()
+        .unwrap()
+        .split_once('=')
+        .expect("token should have been provided as a cookie");
+
+    let mut bearer_token = HeaderValue::from_str(&format!("Bearer {session_id}")).unwrap();
+    bearer_token.set_sensitive(true);
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, bearer_token);
+    let bearer_client = Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+    let res = bearer_client.get(url("/")).send().await.unwrap();
+    assert_eq!(*res.url(), url("/"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log out and check the cookie has been removed in response.
+    let res = client.get(url("/logout")).send().await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
+    assert_eq!(
+        cookie_jar.cookies(&url("/")).iter().len(),
+        0,
+        "Expected 'id' cookie to be removed"
+    );
+
+    // The bearer token should also be invalidated by this point
+    let res = bearer_client.get(url("/")).send().await.unwrap();
+    assert_eq!(*res.url(), url("/login?next=%2F"));
+    assert_eq!(res.status(), StatusCode::OK);
+}
+
+struct ChildGuard {
+    child: Child,
+}
+
+impl Drop for ChildGuard {
+    fn drop(&mut self) {
+        self.child.kill().expect("Failed to kill example binary");
+        self.child
+            .wait()
+            .expect("Failed to wait for example binary to exit");
+    }
+}
+
+async fn start_example_binary(binary_name: &str) -> ChildGuard {
+    let child = Command::new("cargo")
+        .arg("run")
+        .arg("-p")
+        .arg(binary_name)
+        .spawn()
+        .expect("Failed to start example binary");
+
+    let start_time = Instant::now();
+    let mut is_server_ready = false;
+
+    while start_time.elapsed() < Duration::from_secs(300) {
+        if reqwest::get(WEBSERVER_URL).await.is_ok() {
+            is_server_ready = true;
+            break;
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+
+    if !is_server_ready {
+        panic!("The web server did not become ready within the expected time.");
+    }
+
+    ChildGuard { child }
+}
+
+fn url(path: &str) -> Url {
+    let formatted_url = if path.starts_with('/') {
+        format!("{WEBSERVER_URL}{path}")
+    } else {
+        format!("{WEBSERVER_URL}/{path}")
+    };
+    formatted_url.parse().unwrap()
+}
+
+async fn login(client: &Client, username: &str, password: &str) -> reqwest::Response {
+    let mut form = HashMap::new();
+    form.insert("username", username);
+    form.insert("password", password);
+    client.post(url("/login")).form(&form).send().await.unwrap()
+}
